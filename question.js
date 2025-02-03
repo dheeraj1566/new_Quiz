f4f9a7e12685416fae64edbb926810abee285cdc
dheeraj1566 <dheerajjangid013@gmail.com>
2025-02-03 20:28:44 +0530

    first

---
+++ question.js
@@ -0,0 +1,14 @@
+export const questions = [
+    { q: "2+2", opt: [1, 2, 3, 4], correct: 4 },
+    { q: "2*2*2-8", opt: [8, 0, -2, 1], correct: 0 },
+    { q: "3+3+3", opt: [0, 333, 6, 9], correct: 9 },
+    { q: "4*4/4", opt: [4, 0, 8, 16], correct: 4 },
+    {
+      q: '<img class="photo" src="assets/ram mandir.webp" /> <br> Which monument is this?',
+      opt: ["Taj Mahal", "Red Fort", "Qutub Minar", "Ram Mandir"],
+      correct: "Ram Mandir",
+    },
+  ];
+  
+//   export default questions;
+  
